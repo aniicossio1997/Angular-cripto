9184306fd512b242860e0a205ec4dcd5930a5153
ana cossio <anacossio7@gmail.com>
2023-02-06 16:14:32 -0300

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+interface Coin{
+  id: string;
+  name: string;
+  symbol:string;
+  image: string;
+  current_price:number;
+  price_change_percentage_24h:number;
+}  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-dream-app';
+
+
+export class AppComponent implements OnInit{
+  coins:[] =[]
+  constructor(private http:HttpClient){}
+
+  ngOnInit(){
+    this.http
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').subscribe((res) =>{console.log(res)}, (err)=> console.log(err))
+  }
 }
